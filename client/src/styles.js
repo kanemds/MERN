67bed12b002cf48eab64d16c11e5e37a6ee5d60d
Kanem <kanemdeng@email.com>
2022-09-04 03:27:01 -0700

    ade css with material ui

---
+++ client/src/styles.js
@@ -0,0 +1,14 @@
+
+const appBar = {
+  borderRadius: 15,
+  margin: '30px 0',
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'center',
+  alignItems: 'center',
+}
+
+const heading = { color: '#FF2E60' }
+const image = { marginLeft: '15px' }
+
+module.exports = { appBar, heading, image }
